8938266c43c87b57e0449590d253c88a92134ff9
J1nH4ng <jinhang@chinacici.com>
2024-03-28 10:02:40 +0800

    feat(配置文件): 同步配置文件
    Signed-off-by: J1nH4ng <jinhang@chinacici.com>

--- Devops/CI&CD/Jenkins/Config/java-project.sh
+++ Devops/CI&CD/Jenkins/Config/java-project.sh
@@ -47,56 +47,37 @@ function BUILD_ALL_MODULES() {
   for target_name in "${CHILD_MODULE_MEMBERS[@]}"; do
     /usr/bin/cp -rf ./"${target_name}"/target/*.jar ../deploy_tmp/"${JOB_NAME}"/"${target_name}".jar
   done
+  echo "开始同步至应用服务器"
+  ansible "${SERVER}" -m synchronize -a "src=../deploy_tmp/${JOB_NAME} dest=${DIR}/releases/${JOB_NAME}/${BUILD_DISPLAY_NAME}/ compress=yes delete=yes recursive=yes dirs=yes archive=no" -u nginx
+  ansible "${SERVER}" -m file -a "src=${DIR}/releases/${JOB_NAME}/${BUILD_DISPLAY_NAME}/${JOB_NAME} dest=${DIR}/content/${JOB_NAME} state=link" -u nginx
+  ansible "${SERVER}" -m shell -a "sudo supervisorctl restart ${JOB_NAME}-{$CHILD_MODULE_NAME_1,$CHILD_MODULE_NAME_2,$CHILD_MODULE_NAME_3}" -u nginx
+  echo "部署完成"
 }
 
 function BUILD_SINGLE_MODULE() {
   mvn clean package -pl "$1" -am
   echo "编译 $1 完成"
   /bin/cp -rf ./"$1"/target/*.jar ../deploy_tmp/"${JOB_NAME}"/"${JOB_NAME}"-"$1".jar
+  echo "开始同步至应用服务器"
+  ansible "${SERVER}" -m synchronize -a "src=../deploy_tmp/${JOB_NAME} dest=${DIR}/releases/${JOB_NAME}/${BUILD_DISPLAY_NAME}/ compress=yes delete=yes recursive=yes dirs=yes archive=no" -u nginx
+  ansible "${SERVER}" -m file -a "src=${DIR}/releases/${JOB_NAME}/${BUILD_DISPLAY_NAME} dest=${DIR}/content/${JOB_NAME} state=link" -u nginx
+  ansibel "${SERVER}" -m shell -a "sudo supervisorctl restart ${JOB_NAME}-$1" -u nginx
 }
 
 case "${METHOD}" in
   "deploy")
     case "${MODULE_NAME}" in
       "all")
-        echo "开始同步至应用服务器"
-        ansible "${SERVER}" -m synchronize -a "src=../deploy_tmp/${JOB_NAME} dest=${DIR}/releases/${JOB_NAME}/${BUILD_DISPLAY_NAME}/ compress=yes delete=yes recursive=yes dirs=yes archive=no" -u nginx
-        ansible "${SERVER}" -m file -a "src=${DIR}/releases/${JOB_NAME}/${BUILD_DISPLAY_NAME}/${JOB_NAME} dest=${DIR}/content/${JOB_NAME} state=link" -u nginx
-        # 验证 Supervisor 文件是否存在
-        ansible "${SERVER}" -m shell -a "[[ -f /etc/supervisord.d/${JOB_NAME}_${CHILD_MODULE_NAME_1}.ini ]]" -u nginx \
-          && ansible "${SERVER}" -m shell -a "[[ -f /etc/supervisord.d/${JOB_NAME}_${CHILD_MODULE_NAME_2}.ini ]]" -u nginx \
-          && ansible "${SERVER}" -m shell -a "[[ -f /etc/supervisord.d/${JOB_NAME}_${CHILD_MODULE_NAME_3}.ini ]]" -u nginx \
-
-        ansible "${SERVER}" -m shell -a "sudo supervisorctl update" -u nginx
-        ansible "${SERVER}" -m shell -a "sudo supervisorctl restart ${JOB_NAME}-{$CHILD_MODULE_NAME_1,$CHILD_MODULE_NAME_2,$CHILD_MODULE_NAME_3}" -u nginx
-        echo "部署完成"
+        BUILD_ALL_MODULES
       ;;
       "only ${CHILD_MODULE_NAME_1}")
-        mvn clean package -pl "${CHILD_MODULE_NAME_1}" -am
-        echo "编译 ${CHILD_MODULE_NAME_1} 完成"
-        /bin/cp -rf ./"${CHILD_MODULE_NAME_1}"/target/*.jar ../deploy_tmp/"${JOB_NAME}"/"${JOB_NAME}"-"${CHILD_MODULE_NAME_1}".jar
-        echo "开始同步至应用服务器"
-        ansible "${SERVER}" -m synchronize -a "src=../deploy_tmp/${JOB_NAME} dest=${DIR}/releases/${JOB_NAME}/${BUILD_DISPLAY_NAME}/ compress=yes delete=yes recursive=yes dirs=yes archive=no" -u nginx
-        ansible "${SERVER}" -m file -a "src=${DIR}/releases/${JOB_NAME}/${BUILD_DISPLAY_NAME} dest=${DIR}/content/${JOB_NAME} state=link" -u nginx
-        ansibel "${SERVER}" -m shell -a "sudo supervisorctl restart ${JOB_NAME}-${CHILD_MODULE_NAME_1}" -u nginx
+        BUILD_SINGLE_MODULE "${CHILD_MODULE_NAME_1}"
       ;;
       "only ${CHILD_MODULE_NAME_2}")
-        mvn clean package -pl "${CHILD_MODULE_NAME_2}" -am
-        echo "编译 ${CHILD_MODULE_NAME_2} 完成"
-        /bin/cp -rf ./"${CHILD_MODULE_NAME_2}"/target/*.jar ../deploy_tmp/"${JOB_NAME}"/"${JOB_NAME}"-"${CHILD_MODULE_NAME_2}".jar
-        echo "开始同步至应用服务器"
-        ansible "${SERVER}" -m synchronize -a "src=../deploy_tmp/${JOB_NAME} dest=${DIR}/releases/${JOB_NAME}/${BUILD_DISPLAY_NAME}/ compress=yes delete=yes recursive=yes dirs=yes archive=no" -u nginx
-        ansible "${SERVER}" -m file -a "src=${DIR}/releases/${JOB_NAME}/${BUILD_DISPLAY_NAME} dest=${DIR}/content/${JOB_NAME} state=link" -u nginx
-        ansibel "${SERVER}" -m shell -a "sudo supervisorctl restart ${JOB_NAME}-${CHILD_MODULE_NAME_2}" -u nginx
+        BUILD_SINGLE_MODULE "${CHILD_MODULE_NAME_2}"
       ;;
       "only ${CHILD_MODULE_NAME_3}")
-        mvn clean package -pl "${CHILD_MODULE_NAME_3}" -am
-        echo "编译 ${CHILD_MODULE_NAME_3} 完成"
-        /bin/cp -rf ./"${CHILD_MODULE_NAME_3}"/target/*.jar ../deploy_tmp/"${JOB_NAME}"/"${JOB_NAME}"-"${CHILD_MODULE_NAME_3}".jar
-        echo "开始同步至应用服务器"
-        ansible "${SERVER}" -m synchronize -a "src=../deploy_tmp/${JOB_NAME} dest=${DIR}/releases/${JOB_NAME}/${BUILD_DISPLAY_NAME}/ compress=yes delete=yes recursive=yes dirs=yes archive=no" -u nginx
-        ansible "${SERVER}" -m file -a "src=${DIR}/releases/${JOB_NAME}/${BUILD_DISPLAY_NAME} dest=${DIR}/content/${JOB_NAME} state=link" -u nginx
-        ansibel "${SERVER}" -m shell -a "sudo supervisorctl restart ${JOB_NAME}-${CHILD_MODULE_NAME_3}" -u nginx
+        BUILD_SINGLE_MODULE "${CHILD_MODULE_NAME_3}"
       ;;
     esac
   ;;
